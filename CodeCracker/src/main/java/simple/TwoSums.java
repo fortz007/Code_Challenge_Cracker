02ec742a754ced6deba9250b17eb406e84b95054
fortz007 <fortuneojih@yahoo.com>
2022-10-28 09:54:46 +0100

    Two sums second solution

--- CodeCracker/src/main/java/simple/TwoSums.java
+++ CodeCracker/src/main/java/simple/TwoSums.java
@@ -21,4 +21,17 @@ public class TwoSums {
         }
         return array;
     }
+
+    public class Solution2 {
+        public static int[] twoSum(int[] numbers, int target) {
+            for (int i = 0; i < numbers.length; i++) {
+                for (int j = i + 1; j < numbers.length; j++) {
+                    if (numbers[i] + numbers[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return numbers;
+        }
+    }
 }
